7b0e27378552d3da31f9f297caac1a870ee51a86
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-08-06 19:44:15 +0000

    Finished logic functionality

--- app/page.js
+++ app/page.js
@@ -1,26 +1,66 @@
 'use client'
 
+// https://youtu.be/HN7FhKPTohA?si=eIBG_5GMkQwUN4SD
 import { useState } from "react";
 
+const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_GRID_COLOUR = "#fff";
+const DEFAULT_SELECTED_COLOUR = "#000"
+
 export default function Home() {
-  const [gridSize, setGridSize] = useState(16);
+  const [gridSize, setGridSize] = useState(DEFAULT_GRID_SIZE);
   const [grid, setGrid] = useState(
-    Array(gridSize * gridSize).fill('#fff')
+    Array(gridSize * gridSize).fill(DEFAULT_GRID_COLOUR)
   );
 
+  const [lastClicked, setLastClicked] = useState(0); // Temporary
+  const [colour, setColour] = useState(DEFAULT_SELECTED_COLOUR);
+
+  const handleCellClick = (index) => {
+    const newGrid = [...grid];
+    //newGrid[index] = "#000";
+    newGrid[index] = grid[index] === colour ? "#fff" : colour;
+    setGrid(newGrid);
+    setLastClicked(lastClicked + 1);
+  };
+
+  const handleGridChange = (newGridSize) => {
+    setGridSize(newGridSize)
+    setGrid(Array(newGridSize * newGridSize).fill(DEFAULT_GRID_COLOUR))
+  };
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-    <div className="grid w-[30rem] h-[30rem]" style={{
-      gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
-    }}>
-      {grid.map((colour, index) => (
-        <div
-          key={index}
-          className="border"
-          style={{ backgroundColor: colour}}
-        ></div>
-      ))}
-    </div>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-3">
+      <h1 className="text-2xl">Pixelart Editor</h1>
+      <p>Pixels changed: {lastClicked}</p>
+      <div className="flex items-center space-x-4">
+        <label>Choose grid size: </label>
+        <input 
+          type="number"
+          value={gridSize}
+          onChange={(e) => handleGridChange(+e.target.value)}
+          className="border rounded p-0.5"
+        />
+        <label>Choose colour: </label>
+        <input
+          type="color"
+          value={colour}
+          onChange={(e) => setColour(e.target.value)}
+          className="border rounded p-0.5"
+        />
+      </div>
+      <div className="grid w-[30rem] h-[30rem]" style={{
+        gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+      }}>
+        {grid.map((colour, index) => (
+          <div
+            key={index}
+            className="border"
+            style={{ backgroundColor: colour, borderColor: "#171717"}}
+            onClick={() => handleCellClick(index)}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
